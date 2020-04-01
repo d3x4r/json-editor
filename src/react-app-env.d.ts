e19424d85139afc7cc6101ef11d05f78dd08a495
Dexer <norton@e1.ru>
2020-04-01 17:50:56 +0500

    remove lodash from dependencies

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="react-scripts" />
-declare module 'lodash.uniqueid';
-declare module 'lodash.omit';
-declare module 'lodash.set';
-declare module 'lodash.unset';
 declare module 'lodash.get';
-declare module 'lodash.isobject';
