ec1213d3afc1c38ac13447434bedcd48678d0573
Dexer <norton@e1.ru>
2020-04-08 17:14:06 +0500

    Updates

---
+++ precache-manifest.77fe6735654291b83765136046002629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de80c2fa56cd4ee1c6d5e276f1934a3d",
+    "url": "/json-editor/index.html"
+  },
+  {
+    "revision": "08cea4e4fda773fca76a",
+    "url": "/json-editor/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "8e6ce10937515d5e1262",
+    "url": "/json-editor/static/css/main.1b90f45e.chunk.css"
+  },
+  {
+    "revision": "08cea4e4fda773fca76a",
+    "url": "/json-editor/static/js/2.f65091c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/json-editor/static/js/2.f65091c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6ce10937515d5e1262",
+    "url": "/json-editor/static/js/main.dba72d29.chunk.js"
+  },
+  {
+    "revision": "d90e977f6eccdb03994d",
+    "url": "/json-editor/static/js/runtime-main.43b14349.js"
+  }
+]);
