d5c18d8dfd8ea019e7283ee9039720bd06b83772
Dexer <norton@e1.ru>
2020-04-15 18:56:06 +0500

    add component to format result

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch } from 'antd';
 import './Result.css';
 
 interface ResultProps {
   data: {};
 }
-const Result: React.FC<ResultProps> = ({ data }) => (
-  <section className="preview-form">
-    <h2 className="preview-form__title">Result</h2>
-    <pre>{JSON.stringify(data, null, 4)}</pre>
-  </section>
-);
+const Result: React.FC<ResultProps> = ({ data }) => {
+  const [minify, setMinify] = useState(false);
+
+  const result = JSON.stringify(data, null, 4);
+  return (
+    <section className="preview-form">
+      <div className="preview-form__header">
+        <h2 className="preview-form__title">Result</h2>
+        <div>
+          <span>Minify: </span>
+          <Switch onChange={() => setMinify(!minify)} />
+        </div>
+      </div>
+      {minify ? <p>{result}</p> : <pre>{result}</pre>}
+    </section>
+  );
+};
 
 export default Result;
